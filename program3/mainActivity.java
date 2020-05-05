735170acad4d456049b3b7047bd30fbff24eabd1
ShakshiPandey77 <49754283+ShakshiPandey77@users.noreply.github.com>
2020-05-05 14:04:05 +0530

    Update mainActivity.java

--- program3/mainActivity.java
+++ program3/mainActivity.java
@@ -1,7 +1,9 @@
-package com.example.myapplication3;
+package com.example.app3_fragment;
+
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -9,51 +11,40 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 public class MainActivity extends AppCompatActivity {
-
-//Defining the Views
-
     EditText e1,e2;
     Button bt;
     Spinner s;
-
-//Data for populating in Spinner
-
-    String [] dept_array={"CSE","ECE","IT","Mech","Civil"};
+    String [] dept_array={"CSE","ISE","ECE","MECH","CIVIL"};
     String name,reg,dept;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//Referring the Views
-        e1= (EditText) findViewById(R.id.editText);
-        e2= (EditText) findViewById(R.id.editText2);
-        bt= (Button) findViewById(R.id.button);
-        s= (Spinner) findViewById(R.id.spinner);
+        e1=(EditText) findViewById(R.id.edit2);
+        e2=(EditText) findViewById(R.id.edit3);
+        bt=(Button) findViewById(R.id.button);
+        s=(Spinner) findViewById(R.id.spinner);
 
-//Creating Adapter for Spinner for adapting the data from array to Spinner
-        ArrayAdapter adapter= new ArrayAdapter(MainActivity.this,android.R.layout.simple_spinner_item,dept_array);
+        ArrayAdapter adapter=new ArrayAdapter(MainActivity.this,android.R.layout.simple_spinner_item,dept_array);
         s.setAdapter(adapter);
 
-//Creating Listener for Button
-        bt.setOnClickListener(new View.OnClickListener() {
+        bt.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick (View v) {
-
-//Getting the Values from Views(Edittext & Spinner)
-
+            public void onClick(View v){
                 name=e1.getText().toString();
                 reg=e2.getText().toString();
                 dept=s.getSelectedItem().toString();
-//Intent For Navigating to Second Activity
-                Intent i = new Intent(MainActivity.this,SecondActivity.class);
-//For Passing the Values to Second Activity
-                i.putExtra("name_key", name);
+
+                Intent i=new Intent(MainActivity.this,SecondActivity.class);
+
+                i.putExtra("name_key",name);
                 i.putExtra("reg_key",reg);
-                i.putExtra("dept_key", dept);
+                i.putExtra("dept_key",dept);
+
                 startActivity(i);
             }
         });
